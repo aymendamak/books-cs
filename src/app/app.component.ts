490f18f57f2a1af3edc98c11ff6904a67e619338
aymendamak <aymendamak2@gmail.com>
2025-02-09 20:24:48 +0100

    Add navigation bar and integrate footer and modal components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { ModalComponent } from './components/modal/modal.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule, ModalComponent, ModalComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'books-cs';
+
+  constructor(private router: Router) {}
+
+  public navigateToPage(page: string) {
+    this.router.navigate([page]);
+  }
 }
