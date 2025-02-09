874057fdea089f7f10d881fc1ec18b9cf563161a
aymendamak <aymendamak2@gmail.com>
2025-02-09 20:20:33 +0100

    Create routes books and authors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BookComponent } from './pages/book/book.component';
+import { AuthorComponent } from './pages/author/author.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'books', component: BookComponent },
+  { path: 'authors', component: AuthorComponent },
+];
