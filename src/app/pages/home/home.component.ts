01966f50092b910cb0a33cd75a547cef57409f6a
aymendamak <aymendamak2@gmail.com>
2025-02-09 20:24:02 +0100

    Create Home Page

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  imports: [RouterLink],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
