54da344e3d182830c9335b2384414ead2a06714b
aymendamak <aymendamak2@gmail.com>
2025-02-09 20:41:13 +0100

    Add Banner component and integrate it into the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BannerComponent } from '../../components/banner/banner.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  imports: [RouterLink],
+  imports: [RouterLink, BannerComponent],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
